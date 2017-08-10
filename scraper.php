3fff676378a55bde82cedff0ed4f2b4c7bceb40e
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-10 11:46:53 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -54,7 +54,7 @@ for($id = 0; $id <= 0; $id++)
 		}
 	  
 	  
-scraperwiki::save_sqlite(array('name'), array('name' => '$id', 'CaseSeries' => $info['num']));
+scraperwiki::save_sqlite(array('caseno'), array('caseno' => $info['caseno']));
 
 	  	
   }
