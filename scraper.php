aed806ca53f7d7e76c17f1544024d1fb61ed6fba
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-29 15:38:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -41,7 +41,7 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 		
 												
 						scraperwiki::save_sqlite(array('name'), 
-									 array('name' => $info['CaseNo']));
+									 array('name' => $CaseNo));
 					}
 						
 					}
@@ -51,8 +51,8 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 				
 			} 
 					}	    
-				/*		
-						scraperwiki::save_sqlite(array('name'), array('name' => $CaseNo , 'InstDte' => $InstDte, 'nstDte1st' => $nstDte1st, 'Status' => $Status, 'CourtName2' => $CourtName2, 'CaseFlDte' => $CaseFlDte, 'RestrCode' => $RestrCode, 'USCode' => $USCode, 'AdvPSide1' => $AdvPSide1, 'AdvPSide2' => $AdvPSide2));
+						
+						/* scraperwiki::save_sqlite(array('name'), array('name' => $CaseNo , 'InstDte' => $InstDte, 'nstDte1st' => $nstDte1st, 'Status' => $Status, 'CourtName2' => $CourtName2, 'CaseFlDte' => $CaseFlDte, 'RestrCode' => $RestrCode, 'USCode' => $USCode, 'AdvPSide1' => $AdvPSide1, 'AdvPSide2' => $AdvPSide2));
 						}
 	 scraperwiki::save_sqlite(array('CaseNo','CaseNo'), 
     array('CaseNo' => $PageLoop, 
