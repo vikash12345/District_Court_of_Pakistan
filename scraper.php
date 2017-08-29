9ebe52a1d843fb8429d8753e02dfa9478baccca7
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-29 14:24:08 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,31 +1,4 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-
-
-
 require 		'scraperwiki.php';
 require 		'scraperwiki/simple_html_dom.php';
 $BaseLink	=	'http://202.61.43.40:8080/';
@@ -68,11 +41,15 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 		
 												
 						scraperwiki::save_sqlite(array('name'), 
-									 array('name' => $info['CaseNo'])
-									
-									)
+									 array('name' => $info['CaseNo']))
 					}
 						
+					}
+					
+				}
+				
+				
+			} 
 					}	    
 				/*		
 						scraperwiki::save_sqlite(array('name'), array('name' => $CaseNo , 'InstDte' => $InstDte, 'nstDte1st' => $nstDte1st, 'Status' => $Status, 'CourtName2' => $CourtName2, 'CaseFlDte' => $CaseFlDte, 'RestrCode' => $RestrCode, 'USCode' => $USCode, 'AdvPSide1' => $AdvPSide1, 'AdvPSide2' => $AdvPSide2));
@@ -103,10 +80,4 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 							
 						
 					
-					}
-					
-				}
-				
-				
-			} 
 ?>
