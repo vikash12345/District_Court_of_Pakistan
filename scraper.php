980e20be51a9c5792072f983bbe2dfe2eafbe77b
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-29 09:40:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -26,34 +26,52 @@
 
 
 
-require 'scraperwiki.php';
-require 'scraperwiki/simple_html_dom.php';
-//
+require 		'scraperwiki.php';
+require 		'scraperwiki/simple_html_dom.php';
+$BaseLink	=	'http://202.61.43.40:8080/';
+$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
+	
+	//	Page pagination
+	for($PageLoop = 1; $PageLoop < 2; $PageLoop++){
 
-/** looping over list of ids of doctors **/
-for($id = 0; $id <= 1; $id++)
-	{
-	 $url = ("http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=".$id);
-	$link2 = file_get_html($url);
-  foreach($link2->find("//*[@id='w0']/table/tbody/tr")as $element){
+		$FinalURL	=	$SiteURL . $PageLoop;
+		$Html		=	file_get_html($FinalURL);
+		$RowNumb	=	-1;
 
-		if(is_object($element))
-	{
+		if ($Html) {
 
-	 	$info['num'] 		= $element->find("td", 0)->plaintext;
-		$info['courtname']  	= $element->find("td", 1)->plaintext;
-		$info['caseno']  	= $element->find("td", 2)->plaintext;
-		$info['status']  	= $element->find("td", 3)->plaintext;
-		 $info['urlbutton']			= $element->find(".//td/button", 0)->value;
-			scraperwiki::save_sqlite(array('num'), array('num' => $info['num'],'courtname' => $info['courtname'],'caseno' => $info['caseno'],'status' => $info['status'],'urlbutton' => $info['urlbutton'] ));
-
-			
-		}
-	  
-	  
-
-		
-  }
-}
-// called "data".
+			//	Paginate all 'View' buttons
+			foreach ($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
+				$RowNumb	+=	1;
+				if ($RowNumb != 0) {
+					$CourtName	=	$element->find('./td[2]', 0);
+					$CaseNumbr	=	$element->find('./td[3]', 0);
+					$CaseStats	=	$element->find('./td[4]', 0);
+					$CaseValue	=	$element->find('./td[5]/button', 0);
+					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
+					$CaseLink	=	str_replace("amp;", "", $CaseLinkR);
+					$DetailPg	=	file_get_html($CaseLink);
+						
+					if ($DetailPg) {
+						//	Assign fields to varilables
+							//This is for Case Details
+					 	$CaseNo				=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 0)->plaintext;
+						 $InstDte			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
+						 $InstDte1st			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
+						 $Status			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
+						echo $CourtName2			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 2)->plaintext;
+						 $CaseFlDte			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
+						 $RestrCode			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
+						 $USCode			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
+						 $AdvPSide1			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
+						 $AdvPSide2			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
+					
+					
+					}
+					
+				}
+				
+				
+			} }
+				}
 ?>
