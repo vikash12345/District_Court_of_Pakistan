38d9d5ad985869012f5b4024f932bd1b2b385c17
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-29 11:54:04 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -76,8 +76,8 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 										)); */
 							scraperwiki::save_sqlite(array('CaseNo','CaseNo'), 
     											array('CaseNo' => $CaseNo, 
-											'CourtName' => (trim($info['CourtName'])), 
-          										'InstDte1st' => (trim($info['InstDte1st']))
+											'CourtName' => $info['CourtName'], 
+          										'InstDte1st' => $info['InstDte1st']
 											      
 											      ));
 						
