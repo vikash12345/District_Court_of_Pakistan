0e56fd91af136576b6515d05592a38263f5ca019
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-08 15:56:10 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -22,7 +22,7 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 					$info['CaseStats']	=	$element->find('./td[4]', 0)->plaintext;
 					
 				if($num != null){	
-scraperwiki::save_sqlite(array('name','num'), array('name' => $info['Courtname']
+scraperwiki::save_sqlite(array('name','num'), array('name' => $info['Courtname'],
 					     'num' => $info['num'] ,
 					     'Casenumbr' => $info['Casenumbr'] ,
 					     'CaseStats' => $info['CaseStats']
