8c694c1b2276fb6fc58af5a005a9e2be8875c65b
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-08 12:59:06 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -17,32 +17,15 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 			foreach ($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
 				$RowNumb	+=	1;
 				if ($RowNumb != 0) {
+					$Num		=	$element->find('./td[1]', 0);
 					$CourtName	=	$element->find('./td[2]', 0);
 					$CaseNumbr	=	$element->find('./td[3]', 0);
 					$CaseStats	=	$element->find('./td[4]', 0);
 					$CaseValue	=	$element->find('./td[5]/button', 0);
 					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
 					$CaseLink	=	str_replace("amp;", "", $CaseLinkR);
-					$DetailPg	=	file_get_html($CaseLink);
-						
-					if ($DetailPg) {
-						//	Assign fields to varilables
-						//This is for Case Details
-					 	 $info['CaseNo']			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 0)->plaintext;
-						 $InstDte			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
-						 $nstDte1st			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
-						 $Status			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
-						 $CourtName2			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 2)->plaintext;
-						 $CaseFlDte			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
-						 $RestrCode			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
-						 $USCode			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
-						 $AdvPSide1			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
-						 $AdvPSide2			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
-		
-												
-						scraperwiki::save_sqlite(array('name'), 
-									 array('name' => $CaseNo));
-					}
+			 scraperwiki::save_sqlite(array('name'), array('Num' => $Num , 'name' => $CourtName , 'InstDte' => $CaseNumbr, 'nstDte1st' => $CaseStats, 'Status' => $CaseValue 'CourtName2' => $CaseLinkR));
+
 						
 					}
 					
