65afdb4b10ad1b669d4572ee1096f6b2f9c4b696
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-11 11:41:30 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,19 +15,22 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 		
 		
 			foreach($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
-			if($element){
-					$num		=	$element->find('./td[1]', 0)->plaintext;
-					$Courtname	=	$element->find('./td[2]', 0)->plaintext;
-					$Casenumbr	=	$element->find('./td[3]', 0)->plaintext;
-					$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
+			
+				echo	$num		=	$element->find('./td[1]', 0)->plaintext;
+				echo	$Courtname	=	$element->find('./td[2]', 0)->plaintext;
+				echo	$Casenumbr	=	$element->find('./td[3]', 0)->plaintext;
+				echo	$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
 					
-				if($num != null){	
-scraperwiki::save_sqlite(array('name','num'), array('name' => $Courtname,
-					     'num' => $num ,
-					     'Casenumbr' => $Casenumbr ,
+				
+				scraperwiki::save_sqlite(array('num'), array('num' => $num,
+                                             'casename' => $casename,
+                                             'Courtname' => $Courtname,
+					     'Casenumbr' => $Casenumbr,
 					     'CaseStats' => $CaseStats
-					     ));
-			}
+                                             ));
+					
+
+			
 						
 			}	
 					
