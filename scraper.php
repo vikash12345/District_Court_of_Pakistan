458d07647ac7b6bf60f41fa6a40989d923870d4e
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-11 11:50:10 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -12,16 +12,16 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 		
 
 			//	Paginate all 'View' buttons
-		
-		
+		if($Html)
+		{
 			foreach($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
 			
 				
-					echo	$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
-			/*		$num		=	$element->find('./td[1]', 0)->plaintext;
+						
+					$num		=	$element->find('./td[1]', 0)->plaintext;
 					$Courtname	=	$element->find('./td[2]', 0);
 					$Casenumbr	=	$element->find('./td[3]', 0)->plaintext;
-				
+					$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
 				
 					
 				
@@ -31,12 +31,12 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 					     'Casenumbr' => $Casenumbr,
 					     'CaseStats' => $CaseStats
                                              ));
-					*/
+					
 
 			
 						
 				
-					
+			}
 				
 				
 				
