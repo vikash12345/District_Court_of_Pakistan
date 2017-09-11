14cbec3f44787bd61ea58624098391892180f3c9
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-11 11:46:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -17,11 +17,11 @@ $SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 			foreach($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
 			
 				
-				
-				echo	$Courtname	=	$element->find('./td[2]', 0);
-			/*	echo	$num		=	$element->find('./td[1]', 0)->plaintext;
-				
 				echo	$Casenumbr	=	$element->find('./td[3]', 0)->plaintext;
+			/*		$num		=	$element->find('./td[1]', 0)->plaintext;
+					$Courtname	=	$element->find('./td[2]', 0);
+
+				
 				echo	$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
 					
 				
