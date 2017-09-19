42aed0a03365750af6842b134d4f8cb28c8066d3
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-19 09:49:01 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -45,9 +45,9 @@ $BaseLink	=	'http://202.61.43.40:8080/';
 					if ($DetailPg!= null) {
 						//	Assign fields to varilables
 							//This is for Case Details
-						echo "$DetailPg...\n";
-						 $CaseNo			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 0)->plantext;
-						// echo "$CaseNo...\n";
+						// echo "$DetailPg...\n";
+						 $CaseNo			=	$DetailPg->find("/html/body/div/table[1]/tbody/tr[1]/td[1]", 0)->plantext;
+						 echo "$CaseNo...\n";
 						 $instdte			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plantext;
 						 $InstDte1st			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plantext;
 						 $Status			=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plantext;
