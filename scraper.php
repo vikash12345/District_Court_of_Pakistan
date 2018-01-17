705c0cffa59d9100117b06b5ecdfdc51c52a4aa0
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-17 12:47:02 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,7 @@
 require 		'scraperwiki.php';
 require 		'scraperwiki/simple_html_dom.php';
 $BaseLink	=	'http://202.61.43.40:8080/';
-	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=0';
+	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page=0';
 	sleep(5);
 echo $SiteURL;
 	$Pagination = 	file_get_html($SiteURL);
@@ -16,8 +16,9 @@ echo $SiteURL;
 	
 	
 	//	Page pagination
-	for($PageLoop = 1; $PageLoop < $loop; $PageLoop++){
-	$FinalURL  		=  'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page='.$PageLoop;
+	//$loop
+	for($PageLoop = 1; $PageLoop < 2; $PageLoop++){
+	$FinalURL  		=  'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyfir&page='.$PageLoop;
 		$Html		=	file_get_html($FinalURL);
 		sleep(10);
 		$RowNumb	=	-1;
